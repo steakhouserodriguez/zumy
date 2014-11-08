61af6bd231f6e8966e96b0dd7e2b46f288166561
Andrew J. Chen <chenjandrew@gmail.com>
2014-11-07 22:09:43 -0800

    running zumy_lcm_node.py as root

--- start_scripts/odroid_init.sh
+++ start_scripts/odroid_init.sh
@@ -5,6 +5,8 @@
 
 su -l bml -c 'screen -S linux_state -d -m python /home/bml/zumy/python/linux_state_pub.py'
 
-su -l bml -c 'screen -S zumy_lcm_node -d -m python /home/bml/zumy/python/zumy_lcm_node.py'
+#su -l bml -c 'screen -S zumy_lcm_node -d -m python /home/bml/zumy/python/zumy_lcm_node.py'
+
+screen -S netstarter -d -m python /home/bml/zumy/python/zumy_lcm_node.py
 
 screen -S netstarter -d -m python /home/bml/zumy/python/netstarter.py
